739f619b5b3ffd8d4b8c368a2050e4bae057728f
Jay nariya <jaynariya605@gmail.com>
2023-01-13 14:26:27 -0500

    src/generated1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const Post = require('./resolvers/Post')
 const User = require('./resolvers/User')
 const Comment = require('./resolvers/Comment')
 const Subscription = require('./resolvers/Subscription')
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('./generated/client')
 const express = require('express')
 
 const app = express()
